673f63c57ef6a31d78dcc0cdaec9d853efdfb024
gilko <gil.kovary@gmail.com>
2024-09-09 15:19:23 +0300

    test

--- csv_data_pipeline.py
+++ csv_data_pipeline.py
@@ -19,7 +19,7 @@ class Pipeline:
         self.name = "csv_pipeline"
         # Initialize rate limits
         self.valves = self.Valves(**{"OPENAI_API_KEY": os.getenv("OPENAI_API_KEY", "")})
-        self.df = pd.read_csv('data/titanic.csv')
+        # self.df = pd.read_csv('data/titanic.csv')
 
 
     async def on_startup(self):
@@ -42,7 +42,8 @@ class Pipeline:
             print("Title Generation")
             return "CSV Pipeline"
         else:
-            context = self.df.head(3)
+            contex= 'lala and baba'
+            # context = self.df.head(3)
 
             return context if context else "No information found"
 
